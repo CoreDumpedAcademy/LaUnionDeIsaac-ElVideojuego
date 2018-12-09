506ba3b6c88e192b5674c03c54517fbab41ddfd0
limonultra <mayuri.furude@gmail.com>
2018-12-09 19:02:10 +0100

    changes in miniboss && adding of sprites && animations

---
+++ Assets/Scripts/WBossProjectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WBossProjectile : MonoBehaviour {
+
+    private float speed=5;
+
+    private Transform player;
+    private Vector2 target;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+         target = new Vector2(player.position.x, player.position.y);
+        //    En caso de que queramos que el proyectil no siga al jugador sino que se lance hacia su dirección, si no: player.position.x && player.position.y
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            DestroyProjectile();
+        }
+    }
+
+    private void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
