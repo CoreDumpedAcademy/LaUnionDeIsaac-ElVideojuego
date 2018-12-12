537cee33173fed4419ec5652b9ca438fa0a4acd5
TebionDL <purreren@gmail.com>
2018-12-13 00:34:33 +0100

    Added portal mechanics

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour {
+
+    public bool notInMap = true;
+    private float count = 0.2f;
+    public int floorCounter = 1;
+    public int actualFloor;
+    Animator anim;
+
+
+    // Use this for initialization
+    void Start () {
+        //inicializamos variables y pillamos componentes
+        notInMap = true;
+        anim = GetComponent<Animator>();
+        actualFloor = PlayerPrefs.GetInt("actualFloor");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //detecta si el jugador tiene la llave para cambiar la animación de cerrado a abierto
+        if (Player.hasKey)
+        {
+            anim.SetBool("Unlocked", true);
+        }
+        else
+        {
+            anim.SetBool("Unlocked", false);
+        }
+
+        //Si el portal al aparecer no aparece en el mapa, este se borra y se genera otro
+        count = count - Time.deltaTime;
+
+        if (count <= 0 && notInMap == true)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    //detecta si está tocando o no el mapa
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            notInMap = false;
+        }
+    }
+
+    //permite pasar al siguiente piso si el jugador tiene la llave
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Player.hasKey)
+        {
+            SceneManager.LoadSceneAsync("Floor"+actualFloor);
+            floorCounter++;
+            PlayerPrefs.SetInt("actualFloor",floorCounter);
+        }
+    }
+}
---
+++ Assets/Scripts/PortalGenerator.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalGenerator : MonoBehaviour {
+
+    public int numberPortals = 1;
+    public int PortalsSpawned;
+    private float x;
+    private float y;
+    private float z;
+
+    private bool isColliding;
+    public GameObject[] gos;
+
+    private Vector3 pos;
+    public GameObject[] obj;
+    public bool maxPortalsInstantiated = false;
+
+    public float count = 0.2f;
+    public float count2 = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        numberPortals = 1;
+        PortalsSpawned = 0;
+        count = 0.1f;
+        count2 = 1f;
+        maxPortalsInstantiated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        count = count - Time.deltaTime;
+        if (count <= 0)
+        {
+            PortalSpawn();
+        }
+
+
+
+        Debug.Log(gos.Length);
+    }
+    //Función que hace aparecer portales
+    void Spawn()
+    {
+        Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
+    }
+
+    //Función que elige la posición en la que aparecen los portales
+    public void PortalSpawn()
+    {
+        gos = GameObject.FindGameObjectsWithTag("Portal");
+        if (gos.Length < numberPortals && maxPortalsInstantiated == false)
+        {
+
+
+            x = Random.Range(-49f, 49f);
+            y = Random.Range(-49f, 49f);
+            z = 0;
+            pos = new Vector3(x, y, z);
+            transform.position = pos;
+
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(transform.position), Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject.tag != "Wall")
+            {
+                Spawn();
+
+            }
+            PortalsSpawned++;
+
+
+        }
+        else
+        {
+            
+            maxPortalsInstantiated = true;
+            
+        }
+    }
+}
