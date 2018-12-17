68a8f81ad5d3e163605117fd4c6688c3762d5e2e
raular4322 <raular98@hotmail.com>
2018-12-17 13:55:22 +0100

    minor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -75,8 +75,6 @@ public class Player : MonoBehaviour {
         //display de la vida del personaje
         percentageOfHealth = playerHealth / maxHealth;
         healthBar.value = percentageOfHealth;
-        Debug.Log(percentageOfHealth);
-        Debug.Log(healthBar.value);
 
         //obtenemos las direcciones que va a obtener el jugador
         float h = Input.GetAxisRaw("Horizontal");
