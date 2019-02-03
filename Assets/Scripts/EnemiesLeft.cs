cf40ebb8881d712f0fedd1e065af73cfbb26700a
TebionDL <purreren@gmail.com>
2019-02-03 02:26:37 +0100

    added enemy counter

---
+++ Assets/Scripts/EnemiesLeft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnemiesLeft : MonoBehaviour
+{
+
+    private TextMeshProUGUI enemyCounter;
+    private int enemiesLeft = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemiesLeft = 0;
+        enemyCounter = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        enemiesLeft = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        enemyCounter.text = enemiesLeft.ToString();
+    }
+}
